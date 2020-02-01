ca7a8b25277d79ec06a304f0a841ab4e5c6aee13
Sudhir Kumar <tech.sudhirk@gmail.com>
2020-02-02 01:29:59 +0530

    latest one

--- logger.js
+++ logger.js
@@ -1,6 +1,7 @@
 var message = "hello sk";
 function showMessage(){
     console.log(message);
+    console.log("hello world")
 }
 module.exports.message = message;
 module.exports.showMessage = showMessage;
